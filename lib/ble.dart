b45e6ce171cbca6410b50b19ab38c3d4f8f28741
octogradiste <colin.berger@outlook.com>
2021-09-20 13:32:03 +0200

    export ble operartions

--- lib/ble.dart
+++ lib/ble.dart
@@ -4,5 +4,6 @@ export 'src/model/scan_result.dart';
 export 'src/model/service.dart';
 export 'src/model/uuid.dart';
 export 'src/ble_device.dart';
+export 'src/ble_operations.dart';
 export 'src/ble_service.dart';
 export 'src/backend/ble_operations_factory.dart';
