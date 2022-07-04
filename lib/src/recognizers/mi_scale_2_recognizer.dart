c11a0dbe9ec96102bb7319332327d2d086ff3a97
octogradiste <colin.berger@outlook.com>
2022-07-04 22:37:42 +0200

    mi scale 2 implements now abstract weight scale and is fully tested

--- lib/src/recognizers/mi_scale_2_recognizer.dart
+++ lib/src/recognizers/mi_scale_2_recognizer.dart
@@ -1,5 +1,3 @@
-import 'dart:typed_data';
-
 import 'package:weight_scale/scale.dart';
 import 'package:weight_scale/src/ble/ble.dart';
 import 'package:weight_scale/src/scales/mi_scale_2.dart';
@@ -8,18 +6,7 @@ class MiScale2Recognizer implements WeightScaleRecognizer {
   @override
   WeightScale? recognize({required ScanResult scanResult}) {
     if (scanResult.device.information.name == "MIBFS") {
-      ByteData data = ByteData.sublistView(scanResult.serviceData.values.first);
-      WeightUnit unit = WeightUnit.unknown;
-      if (data.getUint8(0) % 2 == 1) {
-        // If last bit of first byte is one then the weight is in LBS.
-        unit = WeightUnit.lbs;
-      } else if ((data.getUint8(1) >> 6) % 2 == 0) {
-        // If second bit of second byte is one then the
-        // weight is in Catty (aka UNKNOWN) else in KG.
-        unit = WeightUnit.kg;
-      }
-
-      return MiScale2(bleDevice: scanResult.device, unit: unit);
+      return MiScale2(device: scanResult.device);
     }
     return null;
   }
--- lib/src/scales/mi_scale_2.dart
+++ lib/src/scales/mi_scale_2.dart
@@ -3,63 +3,62 @@ import 'dart:typed_data';
 
 import 'package:weight_scale/src/ble/ble.dart';
 import 'package:weight_scale/scale.dart';
-import 'package:weight_scale/src/scales/simple_weight_scale.dart';
+import 'package:weight_scale/src/scales/abstract_weight_scale.dart';
 
 /// Mi Body Composition Scale 2
-class MiScale2 extends SimpleWeightScale
+class MiScale2 extends AbstractWeightScale
     implements SetUnitFeature, ClearCacheFeature {
-  final Uuid _customService =
-      const Uuid("00001530-0000-3512-2118-0009af100700");
-  final Uuid _scaleConfiguration =
-      const Uuid("00001542-0000-3512-2118-0009af100700");
+  final Characteristic _configCharacteristic;
   final BleDevice _device;
 
-  MiScale2({required BleDevice bleDevice, required WeightUnit unit})
-      : _device = bleDevice,
-        super(
-          bleDevice: bleDevice,
-          unit: unit,
-          characteristicUuid:
-              const Uuid("00002a9c-0000-1000-8000-00805f9b34fb"),
-          serviceUuid: const Uuid("0000181b-0000-1000-8000-00805f9b34fb"),
+  MiScale2({required super.device})
+      : _device = device,
+        _configCharacteristic = Characteristic(
+          deviceId: device.information.id,
+          serviceUuid: const Uuid("00001530-0000-3512-2118-0009af100700"),
+          uuid: const Uuid("00001542-0000-3512-2118-0009af100700"),
         );
 
   @override
-  final String name = "Mi Body Composition Scale 2";
+  final characteristicUuid = const Uuid("00002a9c-0000-1000-8000-00805f9b34fb");
 
   @override
-  final String manufacturer = "Xiaomi";
+  final serviceUuid = const Uuid("0000181b-0000-1000-8000-00805f9b34fb");
 
   @override
-  Weight? Function(Uint8List) get onData => _onData;
+  final name = "Mi Body Composition Scale 2";
 
-  Weight? _onData(Uint8List value) {
+  @override
+  final manufacturer = "Xiaomi";
+
+  @override
+  Weight? onData(Uint8List data) {
     late WeightUnit unit;
-    ByteData data = ByteData.sublistView(value);
-    if (data.lengthInBytes != 13) return null;
-    unit = WeightUnit.unknown;
-    if (data.getUint8(0) % 2 == 1) {
-      // If last bit of first byte is one then the weight is in LBS.
-      unit = WeightUnit.lbs;
-    } else if ((data.getUint8(1) >> 6) % 2 == 0) {
-      // If second bit of second byte is one then the
-      // weight is in Catty (aka UNKNOWN) else in KG.
-      unit = WeightUnit.kg;
-    }
+    ByteData bytes = ByteData.sublistView(data);
+    if (bytes.lengthInBytes != 13) return null;
 
-    switch (unit) {
-      case WeightUnit.kg:
-        return Weight(data.getUint16(11, Endian.little) / 200, unit);
-      case WeightUnit.lbs:
-        return Weight(data.getUint16(11, Endian.little) / 100, unit);
-      case WeightUnit.unknown:
-        return Weight(0, unit);
+    if (bytes.getInt8(0) & 1 != 0) {
+      // Weight is in lbs.
+      return Weight(bytes.getUint16(11, Endian.little) / 100, WeightUnit.lbs);
+    } else if (bytes.getInt8(1) & 0x40 != 0) {
+      // Weight is in catty.
+      return null;
+    } else {
+      // Weight is in kg.
+      return Weight(bytes.getUint16(11, Endian.little) / 200, WeightUnit.kg);
     }
   }
 
   @override
+  bool hasStabilized(Uint8List data) {
+    ByteData bytes = ByteData.sublistView(data);
+    if (bytes.lengthInBytes != 13) return false;
+    return bytes.getInt8(1) & 0x20 != 0;
+  }
+
+  @override
   Future<void> setUnit(WeightUnit unit) async {
-    Uint8List? value;
+    Uint8List value;
     switch (unit) {
       case WeightUnit.kg:
         value = Uint8List.fromList([6, 4, 0, 0]);
@@ -68,24 +67,17 @@ class MiScale2 extends SimpleWeightScale
         value = Uint8List.fromList([6, 4, 0, 1]);
         break;
       case WeightUnit.unknown:
-        value = null;
-        break;
+        return;
     }
 
-    if (value != null) {
-      try {
-        await _device.writeCharacteristic(
-          Characteristic(
-            deviceId: _device.information.id,
-            serviceUuid: _customService,
-            uuid: _scaleConfiguration,
-          ),
-          value: value,
-          response: false,
-        );
-      } on BleException catch (e) {
-        throw WeightScaleException(e.message);
-      }
+    try {
+      await _device.writeCharacteristic(
+        _configCharacteristic,
+        value: value,
+        response: false,
+      );
+    } on BleException catch (e) {
+      throw WeightScaleException(e.message);
     }
   }
 
@@ -93,11 +85,7 @@ class MiScale2 extends SimpleWeightScale
   Future<void> clearCache() async {
     try {
       await _device.writeCharacteristic(
-        Characteristic(
-          deviceId: _device.information.id,
-          serviceUuid: _customService,
-          uuid: _scaleConfiguration,
-        ),
+        _configCharacteristic,
         value: Uint8List.fromList(const [6, 18, 0, 0]),
       );
     } on BleException catch (e) {
