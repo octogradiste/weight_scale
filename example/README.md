79bb65b112c4c7a0d2b1430a796325e244c581ee
fabien <fberger00@gmail.com>
2023-05-18 21:03:46 +0200

    cleaned up

--- example/README.md
+++
@@ -1,3 +0,0 @@
-# example
-
-An example app for the weight_scale package.
--- lib/ui/snack_bar/info_snack_bar.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class InfoSnackBar extends SnackBar {
-  InfoSnackBar(String info)
-      : super(
-          content: Text(info),
-          behavior: SnackBarBehavior.floating,
-        );
-}
